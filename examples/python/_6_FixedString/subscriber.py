ccd86c819e03f17f1cd599ee49772377cc2094d0
Simon Nguyen <simon.ngn@outlook.com>
2025-07-06 13:15:52 -0400

    Added example

---
+++ examples/python/_6_FixedString/subscriber.py
@@ -0,0 +1,15 @@
+from SharedPubSub import *
+## Watchout :
+# If readWait() without timeout is in the main thread,
+# Use ctrl+\ instead of ctrl+c to exit.
+
+subscriber = Subscriber_FixedString2048("FixedString","FixedStringSubscriberPy",True)
+
+while(True):
+    ### Example 1 : Wait for notification ###
+    value = subscriber.readWait()
+    print("SUBSCRIBER :",value.get() if value else "No value in queue")
+
+    ### Example 2 : Wait for notification with a timeout ###
+    value = subscriber.readWait(2000) # in milliseconds
+    print("SUBSCRIBER :",value.get() if value else "No value in queue")
