7898e5e5c23209dac9c1e15d670ed0bf3dfec15f
SimonNGN <simon.ngn@outlook.com>
2025-09-29 15:33:29 -0400

    Reved python version

--- python_package/setup.py
+++ python_package/setup.py
@@ -18,7 +18,7 @@ ext_modules = [
 
 setup(
     name="SharedPubSub",
-    version="1.0.4",
+    version="1.0.7",
     author="Simon Nguyen",
     description="Shared memory Publisher and Subscriber library",
     long_description=long_description,
